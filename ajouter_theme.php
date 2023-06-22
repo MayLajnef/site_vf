6d3d92b10141630523612eef460f4c4bdca4e234
MayLajnef <125302971+MayLajnef@users.noreply.github.com>
2023-06-22 07:29:25 +0200

    Update ajouter_theme.php

--- ajouter_theme.php
+++ ajouter_theme.php
@@ -1,52 +1,64 @@
+
 <!DOCTYPE html>
-<html lang="fr" dir="ltr">
-  <head>
-    <meta charset="utf-8">
+<html lang="en" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <link href="style.css" rel="stylesheet">
+  <title>Ajout d'un thème</title>
+</head>
+<body>
+  <?php
+  include 'connexion.php';
 
-  </head>
-  <body>
-    <?php
-    //Sanity checks
-    $nom = $_POST['name']; // a condition que 'nom' soit le bon 'name'! expliquez.
-    $descriptif = $_POST['descriptif'];
-
-    if (empty($nom) or empty($descriptif))  {
-      die("Le formulaire est incomplet");
-    } ?>
-Les informations du nouveau thème sont
-<ul>
-  <li> <?php echo $_POST['name'] ?></li>
-</ul>
-<ul>
-  <li> <?php echo $_POST['descriptif'] ?></li>
-</ul>
-<?php
-date_default_timezone_set('Europe/Paris');
-$date = date("Y-m-d");
-
-include ('connexion.php');
-
-
-echo "<br> le nom saisie est : $nom";
-$query = "insert into themes values (null,'$nom','0','$descriptif')";
-// Est-ce que cela fonctionne ? Pourquoi ?
-// ci-dessous autre requete prete a etre testee ensuite voir sujet de TP
-// $query = "insert into eleves values (NULL, 'Blanche', 'Neige', '1800-01-01', "."'$date'".")";
-
-echo "<br>$query<br>";
-// important echo a faire systematiquement, c'est impose !
-
-$result = mysqli_query($connect, $query);
-// $query utilise comme parametre de mysqli_query
-// le test ci-dessous est desormais impose pour chaque appel de :
-// mysqli_query($connect, $query);
-if (!$result)
-{
-  echo "<br>pas bon ".mysqli_error($connect);
-  exit;
+  $req_verif_theme_supprime = 'SELECT * FROM themes WHERE nom = "'.$_POST['nom'].'" and supprime = 1 ';
+  $verif_theme_supprime = mysqli_query($connect, $req_verif_theme_supprime);
+  if (empty($_POST['nom']) || empty($_POST['descriptif']) ) 
+  {
+    echo "Le formulaire est incomplet ! ";
+    echo "<input type='button' onclick=\"window.location='ajout_theme.html'\" value='Réessayer d'ajouter un thème' />";
+    exit();
+  }
+  if (!empty($verif_theme_supprime)) { // si le thème a été supprimé dans le passé
+    $req_reactivation_theme = 'UPDATE themes SET supprime = 0 WHERE nom = "'.$_POST['nom'].'"'; // on ressuscite le thème supprimé
+    $reactivation_theme = mysqli_query($connect, $req_reactivation_theme);
+    if (!$$req_reactivation_theme) echo "<br>Erreur: ".mysqli_error($connect);
+    else {
+      echo <<< EOT
+      <br>
+      <p> Le thème a été réactivé ! </p>
+      <table>
+        <tr>
+          <td> <input type='button' onclick=\"window.location='ajout_theme.html'\" value='Ajouter un autre thème' /> </td>
+          <td> <input type='button' onclick=\"window.location='accueil.html'\" value='Accueil' /> </td>
+        </tr>
+      </table>
+      EOT;
+    }
+  }
+  else {
+  $req_ajout_theme = 'insert into themes values ( NULL,"'.$_POST['nom'].'", 0 , "'.$_POST['descriptif'].'")'; // Requête pour ajouter un thème
+  $ajout_theme = mysqli_query($connect, $req_ajout_theme);
+  if (!$ajout_theme)  
+  {
+    echo "La requête a échoué. <br> Erreur : <br>".mysqli_error($connect);
+    echo "<br> <input type='button' onclick=\"window.location='ajout_theme.html'\" value='Réessayer d'ajouter un thème' />";
+    exit();
+  }
+  else {
+    echo <<< EOT
+    <br>
+    <p> Le thème a été ajouté ! </p>
+    <table>
+        <tr>
+          <td> <input type='button' onclick=\"window.location='ajout_theme.html'\" value='Ajouter un autre thème' /> </td>
+          <td> <input type='button' onclick=\"window.location='accueil.html'\" value='Accueil' /> </td>
+        </tr>
+      </table>
+    EOT;
+  }
 }
-mysqli_close($connect);
-?>
+  mysqli_close($connect);
+    ?>
+</body>
 
-  </body>
 </html>
