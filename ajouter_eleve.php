15bc98635e5dc1eb5e31639b1698158feaadcc2d
MayLajnef <125302971+MayLajnef@users.noreply.github.com>
2023-12-07 11:59:34 +0100

    Update ajouter_eleve.php

--- ajouter_eleve.php
+++ ajouter_eleve.php
@@ -6,15 +6,19 @@
   </head>
   <body>
     <?php
-  $choix = $_POST['choix'];
+    $choix = $_POST['choix'];
   if (!isset($choix) || $choix == 1) {
     include('connexion.php');
+
+    $nom = $_POST["nom"];
+    $prenom = $_POST["prenom"];
+    $dateNaiss = $_POST["naissance"];
 
     $nom_echap = mysqli_real_escape_string($connect, $nom);
     $prenom_echap = mysqli_real_escape_string($connect, $prenom);
 
     date_default_timezone_set('Europe/Paris');
-    $date = date('Y-m-d')
+    $date = date('Y-m-d');
 
     $requete_ajout_eleve = "insert into eleves values (null,'$nom_echap','$prenom_echap','$dateNaiss', '$date')";
 
@@ -26,7 +30,7 @@
       echo "<br>Impossible d'ajouter l'élève. La requête a échoué. <br>  ".mysqli_error($connect);
       exit;
     }
-    else 
+    else
     {
        echo "<p>L'élève $nom $prenom né(e) le $dateNaiss a bien été ajouté.</p>";
     }
